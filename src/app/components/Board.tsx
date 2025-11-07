2af33ca5198c247e59271a9c4352f5c320cb2ea9
Zachary <zacharywilliams080802@gmail.com>
2025-11-07 18:39:00 -0500

    random pieces on each square

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -11,8 +11,8 @@ function Board({ state }: { state: BoardState }) {
             <div key={rowIndex} className="flex">
               {row.map((square, colIndex) => {
                 const color = (((rowIndex + colIndex) % 2) == 0 ? "white" : "black") as Color
-                const piece = new Piece("white", "pawn")
-                return <Square key={`${rowIndex}-${colIndex}`} color={color} piece={piece} />
+
+                return <Square key={`${rowIndex}-${colIndex}`} color={color} piece={square} />
               })}
             </div>
           ))
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import { Piece, PieceName, Color, BoardState, BoardRow } from "./types";
 import Board from "./components/Board";
 
+function randomPiece(): Piece {
+  const colors: Color[] = ["white", "black"];
+  const names: PieceName[] = ["king", "queen", "rook", "bishop", "knight", "pawn"];
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  const name = names[Math.floor(Math.random() * names.length)];
+  return new Piece(color, name);
+}
 export default function Home() {
 
   const state: BoardState = []
   for (let i = 0; i < 8; i++) {
     let row: BoardRow = []
     for (let i = 0; i < 8; i++) {
-      row.push(null)
+      row.push(randomPiece())
     }
     state.push(row)
   }
