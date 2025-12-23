adb1c6f2c1f68773181c62618ddfb1baf9a1247d
Zachary <zacharywilliams080802@gmail.com>
2025-12-22 19:50:44 -0500

    square cmpt in board file, started helper func file

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -1,12 +1,32 @@
-import Square from "./Square"
-import { Color, BoardState } from "@/app/types"
+import { Color, Piece, BoardState } from "@/app/types"
+
+interface SquareProps {
+  bgColor: Color,
+  piece: Piece | null
+}
+
+export function Square({ bgColor, piece }: SquareProps) {
+  return (
+    <div
+      className={`aspect-square flex items-center justify-center ${bgColor === "white" ? "bg-(--white)" : "bg-(--black)"}`}>
+      {piece &&
+        <img
+          className="select-none cursor-pointer object-contain"
+          src={piece.svgPath}
+          draggable={false}
+          alt={piece.color + " " + piece.name}
+        />
+      }
+    </div>
+  );
+}
 
 interface BoardProps {
   state: BoardState
   colorOnBottom: Color
 }
 
-export default function Board({ state, colorOnBottom }: BoardProps) {
+export function Board({ state, colorOnBottom }: BoardProps) {
   return (
     <div className="w-full h-full flex items-center justify-center p-4">
       <div className="grid grid-cols-8 aspect-square w-full max-w-full max-h-full border border-gray-800">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,16 @@
 "use client"
 
 import { Piece, PieceName, Color, BoardState, Rank, File } from "./types";
-import Board from "./components/Board";
+import { Board } from "./components/Board";
 import { Plus, Flag, Handshake, ArrowUpDown } from "lucide-react";
 import { useState } from "react";
+import { initialBoardState } from "./helpers"
 
 // User database schema
 // - name, username, email, uuid, circle color, board theme, piece theme
 
 // Game schema
 // - players' ids, timestamp, winner, PGN, final FEN
-
-function indexToRankFile(row: number, col: number): [Rank, File] {
-  const rank: Rank = (8 - row) as Rank;
-  const file: File = String.fromCharCode(97 + col) as File;
-  return [rank, file];
-}
-
-function randomPiece(): Piece {
-  const colors: Color[] = ["white", "black"];
-  const names: PieceName[] = ["king", "queen", "rook", "bishop", "knight", "pawn"];
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  const name = names[Math.floor(Math.random() * names.length)];
-  return new Piece(color, name);
-}
-
-function emptyBoardState(): BoardState {
-  // Board full of nulls
-  const state: BoardState = Array.from({ length: 8 }, () => Array.from({ length: 8 }, () => null));
-  return state;
-}
-
-function initialBoardState(): BoardState {
-  const state: BoardState = Array.from({ length: 8 }, () => Array.from({ length: 8 }, () => null));
-
-  // Pawns
-  for (let i = 0; i < 8; i++) {
-    state[1][i] = new Piece("black", "pawn");
-    state[6][i] = new Piece("white", "pawn");
-  }
-
-  // Rooks
-  state[0][0] = new Piece("black", "rook");
-  state[0][7] = new Piece("black", "rook");
-  state[7][0] = new Piece("white", "rook");
-  state[7][7] = new Piece("white", "rook");
-
-  // Knights
-  state[0][1] = new Piece("black", "knight");
-  state[0][6] = new Piece("black", "knight");
-  state[7][1] = new Piece("white", "knight");
-  state[7][6] = new Piece("white", "knight");
-
-  // Bishops
-  state[0][2] = new Piece("black", "bishop");
-  state[0][5] = new Piece("black", "bishop");
-  state[7][2] = new Piece("white", "bishop");
-  state[7][5] = new Piece("white", "bishop");
-
-  // Queens
-  state[0][3] = new Piece("black", "queen");
-  state[7][3] = new Piece("white", "queen");
-
-  // Kings
-  state[0][4] = new Piece("black", "king");
-  state[7][4] = new Piece("white", "king");
-
-  return state;
-}
 
 export default function Home() {
 
